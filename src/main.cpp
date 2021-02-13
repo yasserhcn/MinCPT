7ecc57814cd35c56d36906c76a907cba0229b349
yasserhcn <hacini_yasser@yahoo.fr>
2021-02-13 21:30:32 +0100

    added a build argument

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,25 @@
 int main(int argc, char *argv[])
 {
     std::string fileName;
+    std::string arguments;
+    bool shouldBuild = true;
 
-    if(argc == 2)
+    std::cout<<argc;
+    if(argc >= 2)
     {
         fileName = argv[1];
+        
+        for (int i = 2; i < argc; i++)
+        {
+            if(argv[i] == "--build")
+            {
+                shouldBuild = true;
+            }else if(argv[i] == "--no-build")
+            {
+                shouldBuild = false;
+            }
+        }
+        
     }else
     {
         fileName = "build.txt";
@@ -26,6 +41,12 @@ int main(int argc, char *argv[])
 
     std::string command;
     makeCommand(data, &command, &files);
+    std::cout<<command;
+
+    if(shouldBuild)
+    {
+        system(command.c_str());
+    }
     
     return 0;
 }
