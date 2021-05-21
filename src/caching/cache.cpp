b48667a299a08b02ef54f17422051f8df45ebb96
yasserhcn <hacini_yasser@yahoo.fr>
2021-05-21 18:28:53 +0100

    started making cache option

---
+++ src/caching/cache.cpp
@@ -0,0 +1,8 @@
+#include "cache.hpp"
+
+void makeCache(std::vector<data> *dataIn){
+    std::ofstream cacheFile("CPTBuild.txt");
+
+    
+
+}
---
+++ src/caching/cache.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../buildSystem.hpp"
+#include <vector>
+#include <fstream>
+
+// function to convert the data vector into a file
+void makeCache(std::vector<data> *dataIn);
