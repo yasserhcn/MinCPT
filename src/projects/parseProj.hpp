63336aa3d3235cbff11806a88fd56f585d5dd3e3
yasserhcn <hacini_yasser@yahoo.fr>
2021-07-04 21:04:25 +0100

    added parseProj files

---
+++ src/projects/parseProj.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+//TODO : amount of projects in the file
+
+//TODO : function to get the project starting and ending thingy
+
+//TODO : function that returns the text in beteen the proj stuff
