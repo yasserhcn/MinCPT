517a0977250f08c51b7174afe1fe98238255874e
yasserhcn <hacini_yasser@yahoo.fr>
2021-07-05 19:35:31 +0100

    added function to calculate n of projects

--- src/projects/parseProj.cpp
+++ src/projects/parseProj.cpp
@@ -1,1 +1,24 @@
 #include "parseProj.hpp"
+
+int getNumProjects(std::string text)
+{
+    int lines = getOccurences(text, '\n');
+    int beginning = 0, projects = 0;
+
+    for (int i = 0; i < lines; i++)
+    {
+        std::string line = text.substr(beginning, getCharIndex(text, '\n', i));
+        beginning = getCharIndex(text, '\n', i) + 1;
+
+        if(line.substr(0, 7) == "PROJECT")
+        {
+            projects++;
+        }
+    }
+    return projects;
+}
+
+std::pair<int ,int> getIndicesProject(std::string text, int occurence)
+{
+
+}
--- src/projects/parseProj.hpp
+++ src/projects/parseProj.hpp
@@ -1,7 +1,12 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include "../strings.hpp"
 
-//TODO : amount of projects in the file
+
+int getNumProjects(std::string text);
 
 //TODO : function to get the project starting and ending thingy
+std::pair<int ,int> getIndicesProject(std::string text, int occurence);
 
 //TODO : function that returns the text in beteen the proj stuff
