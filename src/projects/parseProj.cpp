ab87b1b51d2c5587d1c7fb4a5c0b9462e54a3998
yasserhcn <hacini_yasser@yahoo.fr>
2021-07-06 21:31:51 +0100

    added getProjectOcc function

--- src/projects/parseProj.cpp
+++ src/projects/parseProj.cpp
@@ -18,7 +18,31 @@ int getNumProjects(std::string text)
     return projects;
 }
 
-std::pair<int ,int> getIndicesProject(std::string text, int occurence)
+int getProjectOcc(std::string text, int occurence)
 {
+    int occ = 0;
 
+    int lines = getOccurences(text, '\n');
+    int beginning = 0;
+
+    for (int i = 0; i < lines; i++)
+    {
+        std::string line = text.substr(beginning, getCharIndex(text, '\n', i));
+        beginning = getCharIndex(text, '\n', i) + 1;
+
+        if(line.substr(0, 7) == "PROJECT")
+        {
+            if(occ == occurence){
+                return getCharIndex(text, '\n', i);
+            }
+            occ++;
+        }
+    }
+    return -1;
+
+}
+
+std::string getProjectText(std::string text, int num)
+{
+    
 }
--- src/projects/parseProj.hpp
+++ src/projects/parseProj.hpp
@@ -3,10 +3,9 @@
 #include <iostream>
 #include "../strings.hpp"
 
+int getProjectOcc(std::string text, int occurence);
 
 int getNumProjects(std::string text);
 
-//TODO : function to get the project starting and ending thingy
-std::pair<int ,int> getIndicesProject(std::string text, int occurence);
-
 //TODO : function that returns the text in beteen the proj stuff
+std::string getProjectText(std::string text, int num);
